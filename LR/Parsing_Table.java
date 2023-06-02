35b4db6ab7835472dce0446c858c488ee89adec8
Isabel Solano <misolanob@gmail.com>
2023-06-02 02:33:20 -0600

    reduce incompleted

--- LR/Parsing_Table.java
+++ LR/Parsing_Table.java
@@ -12,10 +12,21 @@ public class Parsing_Table {
         ArrayList<String> nter = new ArrayList<>();
         ArrayList<String> ter = new ArrayList<>();
 
+
         for (Production p: automata.getProductions()) {
-            if (p.isTerminal()) ter.add(p.getName());
-            else nter.add(p.getName());
+            if (p.isTerminal()) {
+                if (!ter.contains(p.getName())) ter.add(p.getName());
+            }
+            else {
+                // skip the augmented grammar
+                if (!p.getName().equals(automata.getProductions().get(0).getName())){
+                    if (!nter.contains(p.getName())) nter.add(p.getName());
+                }
+            }
         }
+
+        // Add ens symbol
+        ter.add("$");
 
         String[] nonTerminals = nter.toArray(new String[0]);
         String[] terminals = ter.toArray(new String[0]);
@@ -27,11 +38,14 @@ public class Parsing_Table {
         }
         String[] states = st.toArray(new String[0]);
 
+        System.out.println("\nterminals: ");
         System.out.println(Arrays.toString(terminals));
+        System.out.println("\nnon-terminals");
         System.out.println(Arrays.toString(nonTerminals));
+        System.out.println("\nState numbers");
         System.out.println(Arrays.toString(states));
         
-        String[][] TABLA = new String[states.length][nonTerminals.length + terminals.length];
+        String[][] TABLE = new String[states.length][nonTerminals.length + terminals.length];
 
 
         // Shifts
@@ -44,9 +58,38 @@ public class Parsing_Table {
             boolean isTerminal = (pos < terminals.length)? true: false;
 
             // replace in the table
-            if (isTerminal) TABLA[t.getInitialState().getStateNum()][pos] = "s" + Integer.toString(t.getFinalState().getStateNum());
-            else TABLA[t.getInitialState().getStateNum()][pos] = Integer.toString(t.getFinalState().getStateNum());
+            if (isTerminal) TABLE[t.getInitialState().getStateNum()][pos] = "s" + Integer.toString(t.getFinalState().getStateNum());
+            else TABLE[t.getInitialState().getStateNum()][pos] = Integer.toString(t.getFinalState().getStateNum());
 
+        }
+
+        // Reduce
+        for (State s: automata.getStates()) {
+            for (Production p: s.getProductions()) {
+
+                if (p.getProduce().get(p.getProduce().size() - 1).equals(".")) {
+                    // . is the last thing it that production
+                    System.out.println(p);
+                    
+                    // let's find it´s corresponding productino
+                    int Ipos = getEqualProduction(p, automata.getProductions());
+
+                    System.out.println(automata.FOLLOW2(p.getName()));
+
+                    // get follow of the left side of the production
+                    for (String X: automata.FOLLOW2(p.getName())) {
+                        int Spos = getPosition(X, terminals, nonTerminals);
+
+                        System.out.println(p.getName() + ", " +X);
+                        System.out.println(Spos);
+                        
+                        // Add to the table
+                        TABLE[s.getStateNum()][Spos] = "r" + Integer.toString(Ipos);
+                        
+
+                    }
+                } 
+            }
         }
 
 
@@ -57,7 +100,7 @@ public class Parsing_Table {
 
         System.out.println();
 
-        System.out.printf("|%-12s|", "");
+        System.out.printf("|%-4s|", "");
         for (int i = 0; i < terminals.length; i ++){
             System.out.printf("%-12s|", terminals[i]);
         }
@@ -69,10 +112,11 @@ public class Parsing_Table {
 
         System.out.println();
         
-        for (int i = 0; i < TABLA.length; i ++) {        
-            System.out.printf("|%-12s|", states[i]);    
-            for (int j = 0; j < TABLA[0].length; j++) {
-                System.out.printf("%-12s|", TABLA[i][j]);
+        for (int i = 0; i < TABLE.length; i ++) {        
+            System.out.printf("|%-4s|", states[i]);    
+            for (int j = 0; j < TABLE[0].length; j++) {
+                if (TABLE[i][j] == null) System.out.printf("%-12s|", "-");
+                else System.out.printf("%-12s|", TABLE[i][j]);
             }
             System.out.println();
         }
@@ -84,6 +128,9 @@ public class Parsing_Table {
 
         // Checks first the terminals
         for (int i = 0; i < terminals.length; i++) {
+            System.out.println(X+", "+terminals[i] + " " + Integer.toString(X.length()) + " " + Integer.toString(terminals[i].length()));
+            System.out.println();
+            System.out.println(terminals[i].length());
             if (X == terminals[i]) return pos; // Break when found
             else pos ++;
         }
@@ -97,5 +144,35 @@ public class Parsing_Table {
         return pos;
     }
 
+    /**
+     * 
+     * @param pa    Augemnted production
+     * @param po    Original production
+     * @return
+     */
+    private boolean equalProductions(ArrayList<String> pa, ArrayList<String> po) {
+
+        pa.remove(".");
+
+        if (pa.size() != po.size()) return false;
+
+        for (int i = 0; i < po.size(); i++ ) {
+            if (!pa.get(i).equals(po.get(i))) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    private int getEqualProduction(Production p, ArrayList<Production> productions) {
+
+
+        for (int i = 0; i < productions.size(); i++) {
+            if (equalProductions(p.getProduce(), productions.get(i).getProduce())) return i;
+        }
+
+        return -1;
+    }
     
 }
